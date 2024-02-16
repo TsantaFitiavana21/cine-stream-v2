7f2e517fea145495f3ab2146843762a78c5f05c8
Tsanta Fitiavana <tsanta.ratolojanahary@versusmind.eu>
2024-02-16 07:42:05 +0300

    feat 🚀: implement movie card

---
+++ src/components/MovieCard/MovieCard.tsx
@@ -0,0 +1,38 @@
+import "./MovieCardStyle.scss"
+import StarOutlinedIcon from "@mui/icons-material/StarOutlined"
+
+export const MovieCard = ({
+    duration,
+    image,
+    title,
+    nbStars,
+    year,
+}: MovieCardProps) => {
+    return (
+        <div className="movie_card__container">
+            <div className="image">
+                <img src={image} alt="movie picture" />
+            </div>
+            <div className="summary">
+                <div>{title}</div>
+                <div className="stars">
+                    {Array(nbStars).fill(nbStars).map((_, index) => (
+                        <StarOutlinedIcon className="star__icon" key={index} />
+                    ))}
+                </div>
+                <div className="info">
+                    <div className="year">{year}</div>
+                    <div className="time">{duration}</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+type MovieCardProps = {
+    image: string
+    title: string
+    nbStars: number
+    year: string
+    duration: string
+}
--- src/components/RightBar/RightBar.tsx
+++ src/components/RightBar/RightBar.tsx
@@ -1,15 +1,30 @@
 import { AvatarBox } from "../AvatarBox/AvatarBox"
+import { MovieCard } from "../MovieCard/MovieCard"
 import "./rightbarStyle.scss"
-import profileImg from "../../../public/assets/img/profile.jpg"
 
 export const RightBar = () => {
     return (
         <div className="right_main">
             <AvatarBox
                 email="test@test.com"
-                image={profileImg}
+                image={"/assets/img/profile.jpg"}
                 name="Tsanta Ratolojanahary"
             />
+
+            <div className="title">Top Movies</div>
+
+            <div className="list">
+                {[1, 2, 3].map((_, index) => (
+                    <MovieCard
+                        key={index}
+                        duration="2h30"
+                        image="/assets/img/kong.webp"
+                        nbStars={2}
+                        title="Kong vs Godzilla"
+                        year="2014"
+                    />
+                ))}
+            </div>
         </div>
     )
 }
